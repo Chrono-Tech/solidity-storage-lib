94ab2a172021d98b3ced1c5d17262aa673f2406d
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-06-13 14:55:36 +0300

    Project: update packages and truffle config

--- truffle.js
+++ truffle.js
@@ -3,13 +3,20 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 4700000,
     },
     test: {
       network_id: 424242,
       host: 'localhost',
       port: 8545,
-      gas: 4700000
+      gas: 4700000,
     }
-  }
+  },
+  solc: {
+    optimizer: {
+        enabled: true,
+        runs: 200,
+    }
+  },
 };
