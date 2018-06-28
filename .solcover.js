f313ae43d99fc5ee8f78ed9b9aac16e4a3aa3862
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2018-06-28 17:42:20 +0300

    MINT-1637 Update shared libs #3comment Upgrade deps. Add solidity coverage.

---
+++ .solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+  copyNodeModules: true,
+  skipFiles: [
+      'Migrations.sol',
+      'stubs/FakeAccessManager.sol',
+      'stubs/StorageManagerStub.sol',
+      'stubs/StorageTester.sol'
+  ]
+}
